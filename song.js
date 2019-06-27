8de23c41de144ad47ff735430f282b785029b750
jforjava1981 <jforjava1981@gmail.com>
2019-06-27 15:07:29 +0530

    Initial commit

---
+++ song.js
@@ -0,0 +1,31 @@
+var name = "You fill up my senses"
+var author = "John Denver"
+var dateWritten = "1972-5-15"
+var composers = ["John Denver"]
+var duration  = "10800"
+var theme = "romantic"
+var genre = "classic"
+var singers = { "name": "John", "lastName": "Denver", "name": "Sara", "lastName": "Denver" } 
+var availableOnCD = false
+var availableOnAppleItunes = true
+var formatsAvailable = ["mp3","mp4","itunes"]
+var dateRecored = "1972-5-25"
+var recordingStudioName = "Jingle Bell Studios"
+var copyRightOwners = [
+  { 
+    "ownerType" : "personal", 
+    "name": "Jenny", 
+    "lastName":"Denver"
+  },
+  {
+    "ownerType" : "business", 
+    "name": "Denver Studios" 
+  }]
+ var copyRightValidTill = "2019-12-31"
+ var numberofStanza = 4
+ var language = "English"
+ var isTranlationAvailableInOtherLanguages = false
+ var copiesSold = "1000000"
+ var album = "NA"
+ var isSingle = true
+ 

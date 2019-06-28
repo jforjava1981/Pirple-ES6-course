c8487e7e95a0dd46f4830ec289788be30ae6b504
jforjava1981 <jforjava1981@gmail.com>
2019-06-28 13:09:50 +0530

    ES6 Exercise # 2 Variables and Constants - add explaination and comments to distinguish between exercises

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import { printSongInfo } from './song.js';
 import { letExample, varExample, constExample } from './variables.js';
+
+//Exercise # 1 - Data Types
 printSongInfo();
+
+//Exercise # 2 - Variables and Constants
 varExample();
 letExample();
 constExample();
+
+//Exercise # 3 - 
--- variables.js
+++ variables.js
@@ -16,12 +16,13 @@
 
 
 /** 
- * 'var', 'const' and 'let' are used to declare containers for values usually known as    variables.
+ * 'var', 'const' and 'let' are used to declare containers for values usually known as    variables/constants.
  * identifiers declared using 'var' and 'let' can be assigned different values at later point in time since their first assignment. 
  * identifiers using const qualifier cannot be re-assigned a value once they are assigned a value. in short once assigned identifier cannot point to any other value.
  * identifiers declared using 'var' or 'let' need not have an initial value when they are declared. However const identifiers must have a value assigned to them when they are declared.
  * identifiers using 'var' qualifier are scoped to a function in which they are declared or to a global scope if decslared at top level in an empty javasript file.
  * identifiers using 'let' or 'const' are scoped to a block in which they are declared.
+ * important difference between 'var' and 'let' is that identifiers using 'var' can be refered before they are declared unlike identifiers using 'let'.
   * so generally when we want to change the values or reuse the same container to store different values at different points of time during the execution of program, we use var or let.
  * Pereferably we use let over var  as it makes the scope clearer and avoids issues with var that might happen when its place of declaration is not aligned with its scope. e.g. in "hoisting". 
  * when we are sure that we dont want to change the value once assigned we use const.
@@ -40,17 +41,18 @@ export function varExample() {
    console.log(name);
 }
 
-// let example
+// let example - defined in block
 export function letExample() {
-   if(true){
+   console.log(name) // not visible here - should give reference error
+   if(true) {
      let name = "merry"
      console.log(name);
    }
-   console.log(name);
+   console.log(name) // - should give reference error
 }
 
 //const example
-export function constExample(){
+export function constExample() {
   const PI = 3.14;
   let r = 2;
   let areaofcircle = PI * r * r;

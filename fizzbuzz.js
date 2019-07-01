1d960e88fe84816f4b5419d25c8d883dfe8b0d30
Shailesh Vaishampayan <jforjava1981@gmail.com>
2019-07-01 13:15:32 +0530

    ES6 Exercise # 6 Loops - create FizzBuzzPrime solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,6 +4,23 @@ export function fizzBuzz() {
       }
 }
 
+export function fizzBuzzPrime() {
+      function isPrime(num) {
+        for(let i = 2; i < num; i++) {
+            if( num % i === 0 ) {
+              return false;
+            }
+        }
+        return true;
+      }
+      for(let i = 1 ; i <= 100; i++) {
+          if(  i !== 1 && isPrime(i) ) {
+            console.log("Prime");  
+          } else {
+            printFizzBuzz(i);  
+          }
+      }
+}
 
 
 export function printFizzBuzz(i) {
